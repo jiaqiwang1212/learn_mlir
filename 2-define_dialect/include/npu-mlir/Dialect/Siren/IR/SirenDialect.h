1f4020ea11ecb356552fb2f71070b85bbfe155dd
wangjiaqi <wangjiaqi@163.com>
2025-08-01 11:36:17 +0800

    02-Dialect tb定义时dependentDialects的使用

--- 2-define_dialect/include/npu-mlir/Dialect/Siren/IR/SirenDialect.h
+++ 2-define_dialect/include/npu-mlir/Dialect/Siren/IR/SirenDialect.h
@@ -1,6 +1,8 @@
 #ifndef NPU_MLIR_SIREN_DIALECT_H
 #define NPU_MLIR_SIREN_DIALECT_H
 
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/MLIRContext.h"
 #include "npu-mlir/Dialect/Siren/IR/SirenDialect.h.inc"
