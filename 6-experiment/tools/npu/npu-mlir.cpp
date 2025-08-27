86086239c27b43e1045ec4a90e9ffb620ad1fa06
wangjiaqi <wangjiaqi@163.com>
2025-08-27 16:46:56 +0800

    06-experiment 使用C++ API解析.mlir文本文件

--- 6-experiment/tools/npu/npu-mlir.cpp
+++ 6-experiment/tools/npu/npu-mlir.cpp
@@ -2,9 +2,12 @@
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/Verifier.h"
 #include "mlir/InitAllDialects.h"
+#include "mlir/Parser/Parser.h"
 #include "mlir/Pass/PassManager.h"
+#include "mlir/Support/FileUtilities.h"
 #include "mlir/Transforms/Passes.h"
 #include "npu-mlir/Dialect/Siren/IR/SirenDialect.h"
+#include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/raw_ostream.h"
 
 int main() {
@@ -18,7 +21,25 @@ int main() {
 
   mlir::OpBuilder builder(&context);
 
-  auto loc = builder.getUnknownLoc();
+  const char *path = "/data1/wangjiaqi/workspace/llvm_essentials/6-experiment/"
+                     "test/test_liveness.mlir";
+  std::string error;
+  auto file = mlir::openInputFile(path, &error);
+  if (!file) {
+    llvm::errs() << "Error opening file: " << error << "\n";
+    return 1;
+  }
+  // llvm::SourceMgr 是LLVM/MLIR中的源代码管理器，用于处理和管理源代码文件
+  llvm::SourceMgr sourceMgr;
+  sourceMgr.AddNewSourceBuffer(std::move(file), llvm::SMLoc());
 
+  mlir::OwningOpRef<mlir::ModuleOp> module =
+      mlir::parseSourceFile<mlir::ModuleOp>(sourceMgr, &context);
+
+  if (!module) {
+    llvm::errs() << "Error parsing MLIR file\n";
+    return 1;
+  }
+  module->dump();
   return 0;
 }
